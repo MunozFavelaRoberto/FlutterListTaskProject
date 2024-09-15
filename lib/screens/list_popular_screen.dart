f1d8cbe63bd93c4b74b8676b347f3ffd519dd982
Roberto Munoz Favela <favelar666@gmail.com>
2024-09-14 23:49:29 -0600

    uploading project files

---
+++ lib/screens/list_popular_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:practica1/models/popular_model.dart';
+import 'package:practica1/network/popular_movies_api.dart';
+
+class ListPopularScreen extends StatefulWidget {
+  ListPopularScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListPopularScreen> createState()=> _ListPopularScreenState();
+}
+
+class _ListPopularScreenState extends State<ListPopularScreen> {
+  PopularMoviesAPI popularAPI = PopularMoviesAPI();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Popular movies'),
+      ),
+      body: FutureBuilder(
+        future: popularAPI.getAllPopular(),
+        builder: (BuildContext context,
+        AsyncSnapshot<List<PopularModel>?> snapshot) {
+          if(snapshot.connectionState == ConnectionState.done)
+            return _listViewPopular(snapshot.data); //si trae datos entramos a regresar el _listViewPopular
+          else if(snapshot.hasError)
+          return Center(child: Text('Ocurrio un error en la petición ${snapshot.error.toString()}'));
+          else
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+
+  Widget _listViewPopular(List<PopularModel>? snapshot) {
+    return ListView.separated(
+      separatorBuilder: (context, index) => Divider(color: Colors.black,), //separador con una linea.
+      padding: EdgeInsets.all(10), //espaciado entre las orillas de la pantalla.
+      itemCount: snapshot!.length, //para que no siga infinitamente, hasta que encuentra algo que ya no existe.
+      itemBuilder: (context, index) {
+        return ClipRRect( //para que aplique el recorte a las imagenes y no solo al contenedor.
+        borderRadius: BorderRadius.circular(10), //borde circular a la imagen.
+          child: Stack(
+            alignment: Alignment.bottomCenter,//pa alinear abajo el stack
+            children: [
+              FadeInImage(
+                fadeInDuration: Duration(milliseconds: 500),
+                placeholder: AssetImage('assets/loading_movie.webp'),
+                image: NetworkImage(
+                  'https://image.tmdb.org/t/p/w500/${snapshot[index].backdropPath!}'),
+              ),
+              Container(
+                color: Colors.black.withOpacity(.6),
+                height: 60,
+                child: ListTile(
+                  onTap: () => Navigator.pushNamed(context, '/detail',
+                      arguments: snapshot[index]),
+                  title: Text(
+                    '${snapshot[index].title}',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  trailing: Icon(
+                    Icons.chevron_right,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+              )
+            ],
+            
+          ),
+          
+        );
+      },
+      );
+  }
+}
